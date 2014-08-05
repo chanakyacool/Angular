b1dd98f831669d3754e33221ca75a92258d06e64
Chanakya <chanakyadevraj@gmail.com>
2014-08-05 12:51:30 +0530

    images folder and ng-src dynamic images

--- app.js
+++ app.js
@@ -11,10 +11,10 @@
 				name: 'Kohinoor',
 				price: 2,
 				description: 'oldest blue daimond',
-				images: [{
-						full: 'http://crazyhits.mobie.in/blog_images/koh-i-noor-diamond-aeaxd4-xl.jpg',
-						thumb: 'http://lh5.ggpht.com/_BfC6j03vee0/TAYvj8r3fyI/AAAAAAAAB1o/ILWtf6HYhYQ/koh-i-noor-diamond_thumb%5B7%5D.jpg?imgmax=800'
-				},
+				images: [ 
+						 "images/kohinoor_small.jpg",
+						  "images/kohinoor_thumb.jpg"
+				 
 				],
 				canPurchase: true
 			},
@@ -22,10 +22,10 @@
 				name: "Pentagonal Gem",
 				price: 5.95,
 				description: "Rare kind of Gem",
-				images: [{
-						full: 'http://cdn2.fast-serve.net/cdn/antiquediamondring/GEMSTONE-CUTS_390_290_587U4.jpg',
-						thumb: 'http://cdn2.fast-serve.net/cdn/antiquediamondring/GEMSTONE-CUTS_390_290_587U4.jpg'
-				},
+				images: [ 
+						 "images/pentagonal_small.jpg",
+						  "images/pentagonal_thumb.jpg"
+				 
 				],
 				canPurchase: false
 			},
@@ -33,10 +33,10 @@
 				name: "Ruby Gem",
 				price: 9.99,
 				description: "Rubary Gem ",
-				images: [{
-						full: 'http://datastore02.rediff.com/h450-w670/thumb/6656615C6B6161656B706668/bmhxdrv0iaf9zd4x.D.0.RUBY.jpg',
-						thumb: 'http://datastore02.rediff.com/h450-w670/thumb/6656615C6B6161656B706668/bmhxdrv0iaf9zd4x.D.0.RUBY.jpg'
-				},
+				images: [ 
+						 "images/ruby_small.jpg",
+						  "images/ruby_thumb.jpg"
+				 
 				],
 				canPurchase: true
 			},
